c1435e823b2b9b2bd9ddc61646449e85c4e6f968
guillempla <guillem.pla@est.fib.upc.edu>
2020-04-08 14:52:55 +0200

    Improved the message and fixed bugs

--- covid-bot.py
+++ covid-bot.py
@@ -1,8 +1,10 @@
 import pandas as pd
 from sodapy import Socrata
+from telegram import ParseMode
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 
+# calculates the number of cases for the county
 def getNumberCases(county):
     county = county
     df_county = df.loc[df['comarcadescripcio'] == county]
@@ -13,23 +15,40 @@ def getNumberCases(county):
         total_tests += int(row['numcasos'])
         if row['resultatcoviddescripcio'] == 'Positiu':
             positive_cases += int(row['numcasos'])
-        elif row['resultatcoviddescripcio' == 'Negatiu']:
+        elif row['resultatcoviddescripcio'] == 'Negatiu':
             negative_cases += int(row['numcasos'])
+    return (str(positive_cases), str(negative_cases), str(total_tests))
 
-    return str(positive_cases)
+
+# send a message to the user with the information of covid
+def printCountyInformation(update, context, county, positive, negative, total):
+    msg = county + ":\n" +\
+        "El nombre de casos positius és de " + positive + "\n" +\
+        "El nombre de casos negatius és de " + negative + "\n" +\
+        "El nombre total de tests que s'ha realitzat és de " + total
+    context.bot.send_message(chat_id=update.message.chat_id,
+                             text=msg, parse_mode=ParseMode.MARKDOWN)
 
 
+# executed when the bot receives a message
 def counties(update, context):
     county = update.message.text
     if county in COUNTIES:
-        cases = getNumberCases(county)
-        msg = "El nombre de casos a " + county + " és de " + cases
-        context.bot.send_message(chat_id=update.message.chat_id, text=msg)
+        positive, negative, total = getNumberCases(county)
+        printCountyInformation(update, context, county, positive, negative, total)
     else:
         fail_text = "Escriu una comarca vàlida si us plau"
         context.bot.send_message(chat_id=update.message.chat_id, text=fail_text)
 
 
+# executed when the /help command is called
+def help(update, context):
+    help_text = open('help.txt').read()
+    context.bot.send_message(chat_id=update.message.chat_id,
+                             text=help_text, parse_mode=ParseMode.MARKDOWN)
+
+
+# executed when the /start command is called
 def start(update, context):
     info_text = "El bot s'ha inicializat correctament!!"
     context.bot.send_message(chat_id=update.message.chat_id, text=info_text)
@@ -51,8 +70,9 @@ df = pd.DataFrame.from_dict(data)
 df['comarcadescripcio'] = df['comarcadescripcio'].str.replace("\xa0", "")
 df.to_pickle("df.pkl")
 
-# when the bot receives the command /start the function start is executed
+# when the bot receives a command its functionis executed
 updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('help', help))
 
 # handling callbacks functions to the commands
 updater.dispatcher.add_handler(MessageHandler(Filters.text, counties))
