80f397122b6e7d7f0d2aa4e0a0128ced22604aee
guillempla <guillem.pla@est.fib.upc.edu>
2020-04-09 11:07:47 +0200

    Fixed update bug and added not strict typing

--- src/covid-bot.py
+++ src/covid-bot.py
@@ -5,9 +5,24 @@ from telegram import ParseMode
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 
+def writeCountyProperly(county):
+    original_counties = [line.strip() for line in open('./text/counties.txt')]
+    return original_counties[COUNTIES.index(county)]
+
+
+def updateDatabase():
+    data = client.get(dataset_id, limit=50000)
+    df = pd.DataFrame.from_dict(data)
+    # dataset contains extra characters on those counties finished with 'à'
+    df['comarcadescripcio'] = df['comarcadescripcio'].str.replace(
+        "\xa0", "")
+    df['comarcadescripcio'] = df['comarcadescripcio'].str.replace("à", "a")
+    df['comarcadescripcio'] = df['comarcadescripcio'].str.replace("è", "e")
+    df['comarcadescripcio'] = df['comarcadescripcio'].str.lower()
+
+
 # calculates the number of cases for the county
 def getNumberCases(county):
-    county = county
     df_county = df.loc[df['comarcadescripcio'] == county]
     total_tests = 0
     positive_cases = 0
@@ -23,7 +38,7 @@ def getNumberCases(county):
 
 # send a message to the user with the information of covid
 def printCountyInformation(update, context, county, positive, negative, total):
-    msg = county + ":\n" +\
+    msg = writeCountyProperly(county) + ":\n" +\
         "El nombre de casos positius és de " + positive + "\n" +\
         "El nombre de casos negatius és de " + negative + "\n" +\
         "El nombre total de tests que s'han realitzat és de " + total
@@ -33,12 +48,14 @@ def printCountyInformation(update, context, county, positive, negative, total):
 
 # executed when the bot receives a message
 def counties(update, context):
+    updateDatabase()
     county = update.message.text
+    county = county.replace("à", "a").replace("è", "e").lower()
     if county in COUNTIES:
         positive, negative, total = getNumberCases(county)
         printCountyInformation(update, context, county, positive, negative, total)
     else:
-        fail_text = "Escriu una comarca vàlida si us plau"
+        fail_text = "Escriu una comarca vàlida si us plau. Consulta a /help les comarques."
         context.bot.send_message(chat_id=update.message.chat_id, text=fail_text)
 
 
@@ -60,8 +77,8 @@ TOKEN = open('./text/token.txt').read().strip()
 updater = Updater(token=TOKEN, use_context=True)
 
 # load Catalan counties list
-COUNTIES = set(line.strip() for line in open('./text/counties.txt'))
-
+COUNTIES = [line.strip().replace("à", "a").replace("è", "e").lower()
+            for line in open('./text/counties.txt')]
 # load the tests dataset's client
 socrata_token = os.environ.get("SODAPY_APPTOKEN")
 client = Socrata("analisi.transparenciacatalunya.cat", socrata_token)
@@ -69,9 +86,11 @@ dataset_id = "jj6z-iyrp"
 data = client.get(dataset_id, limit=50000)
 df = pd.DataFrame.from_dict(data)
 # dataset contains extra characters on those counties finished with 'à'
-df['comarcadescripcio'] = df['comarcadescripcio'].str.replace("\xa0", "")
-df.to_pickle("df.pkl")
-
+df['comarcadescripcio'] = df['comarcadescripcio'].str.replace(
+    "\xa0", "")
+df['comarcadescripcio'] = df['comarcadescripcio'].str.replace("à", "a")
+df['comarcadescripcio'] = df['comarcadescripcio'].str.replace("è", "e")
+df['comarcadescripcio'] = df['comarcadescripcio'].str.lower()
 # when the bot receives a command its functionis executed
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('help', help))
