386c4ec51b985e6904b0ad54f74d5446c64f2c61
guillempla <guillem.pla@est.fib.upc.edu>
2020-04-11 17:00:22 +0200

    Improved query detection

--- src/covid-bot.py
+++ src/covid-bot.py
@@ -1,45 +1,21 @@
 import os
+import difflib
 import pandas as pd
 from sodapy import Socrata
 from telegram import ParseMode
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 
-def cleanString(msg):
-    return msg.strip().lower().replace("à", "a").replace("è", "e").replace("é", "e").replace("í", "i").replace("ó", "o").replace("ò", "o").replace("ú", "u")
-
-
-def writeCountyProperly(region):
-    if region in COUNTIES:
-        original_counties = [line.strip() for line in open('./text/counties.txt')]
-        return original_counties[COUNTIES.index(region)]
-    elif region in MUNICIPALITIES:
-        original_municipalities = [line.strip()
-                                   for line in open('./text/municipalities_complete.txt')]
-        return original_municipalities[MUNICIPALITIES.index(region)]
-
-
+# load the tests dataset's client
 def updateDatabase():
-    # load the tests dataset's client
     client = Socrata(dataset_link, socrata_token)
     data = client.get(dataset_id, limit=LIMIT)
     df = pd.DataFrame.from_dict(data)
     # dataset contains extra characters on those counties finished with 'à'
     df['comarcadescripcio'] = df['comarcadescripcio'].str.replace(
         "\xa0", "")
-    df['comarcadescripcio'] = df['comarcadescripcio'].str.lower()
-    df['comarcadescripcio'] = df['comarcadescripcio'].str.replace("à", "a")
-    df['comarcadescripcio'] = df['comarcadescripcio'].str.replace("è", "e")
     df['municipidescripcio'] = df['municipidescripcio'].str.replace(
         "\xa0", "")
-    df['municipidescripcio'] = df['municipidescripcio'].str.lower()
-    df['municipidescripcio'] = df['municipidescripcio'].str.replace("à", "a")
-    df['municipidescripcio'] = df['municipidescripcio'].str.replace("è", "e")
-    df['municipidescripcio'] = df['municipidescripcio'].str.replace("é", "e")
-    df['municipidescripcio'] = df['municipidescripcio'].str.replace("í", "i")
-    df['municipidescripcio'] = df['municipidescripcio'].str.replace("ó", "o")
-    df['municipidescripcio'] = df['municipidescripcio'].str.replace("ò", "o")
-    df['municipidescripcio'] = df['municipidescripcio'].str.replace("ú", "u")
     return df
 
 
@@ -61,7 +37,7 @@ def getNumberCases(region, descripcio):
 
 # send a message to the user with the information of covid
 def printCountyInformation(update, context, region, positive, negative, total):
-    msg = writeCountyProperly(region) + ":\n" +\
+    msg = region + ":\n" +\
         "El nombre de casos positius és de " + positive + "\n" +\
         "El nombre de casos negatius és de " + negative + "\n" +\
         "El nombre total de tests que s'han realitzat és de " + total
@@ -69,15 +45,25 @@ def printCountyInformation(update, context, region, positive, negative, total):
                              text=msg, parse_mode=ParseMode.MARKDOWN)
 
 
+def typeOfRegion(region):
+    closest_match = difflib.get_close_matches(region, REGIONS, cutoff=0.7)
+    if len(closest_match) != 0:
+        if closest_match[0] in COUNTIES:
+            return (closest_match[0], 0)
+        if closest_match[0] in MUNICIPALITIES:
+            return (closest_match[0], 1)
+    return ("None", -1)
+
+
 # executed when the bot receives a message
 def query(update, context):
     region = update.message.text
     print(region)
-    region = cleanString(region)
-    if region in COUNTIES:
+    region, type = typeOfRegion(region)
+    if (type == 0):
         positive, negative, total = getNumberCases(region, 'comarcadescripcio')
         printCountyInformation(update, context, region, positive, negative, total)
-    elif region in MUNICIPALITIES:
+    elif (type == 1):
         positive, negative, total = getNumberCases(region, 'municipidescripcio')
         printCountyInformation(update, context, region, positive, negative, total)
     else:
@@ -85,18 +71,21 @@ def query(update, context):
         context.bot.send_message(chat_id=update.message.chat_id, text=fail_text)
 
 
+# executed when the /comarques command is called
 def comarques(update, context):
     counties_text = open('./text/comarques.txt').read()
     context.bot.send_message(chat_id=update.message.chat_id,
                              text=counties_text, parse_mode=ParseMode.MARKDOWN)
 
 
+# executed when the municipis command is called
 def municipis(update, context):
-    municipalities_text = "Per a veure el llistat de municipis clica al següent enllaç:\n" + \
+    municipalities_text = "Per a veure el llistat de municipis clica al següent enllaç:\n" +\
         "https://github.com/guillempla/catalan-covid-tests-bot/blob/master/text/municipalities_complete.txt"
     context.bot.send_message(chat_id=update.message.chat_id, text=municipalities_text)
 
 
+# executed when the /help command is called
 def help(update, context):
     help_text = open('./text/help.txt').read()
     context.bot.send_message(chat_id=update.message.chat_id,
@@ -114,13 +103,16 @@ TOKEN = open('./text/token.txt').read().strip()
 updater = Updater(token=TOKEN, use_context=True)
 
 # load Catalan counties list
-COUNTIES = [cleanString(line) for line in open('./text/counties.txt')]
-MUNICIPALITIES = [cleanString(line) for line in open('./text/municipalities_complete.txt')]
+COUNTIES = [line.strip() for line in open('./text/counties.txt')]
+MUNICIPALITIES = [line.strip() for line in open('./text/municipalities_complete.txt')]
+REGIONS = [line.strip() for line in open('./text/regions.txt')]
+
 # load the socrata token and the dataset_id
 socrata_token = os.environ.get("SODAPY_APPTOKEN")
 dataset_link = "analisi.transparenciacatalunya.cat"
 dataset_id = "jj6z-iyrp"
 LIMIT = 50000
+
 # when the bot receives a command its functionis executed
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('help', help))
